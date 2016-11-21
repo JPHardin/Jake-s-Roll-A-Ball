90da98a4538e42c2a91f16ff3a44322bb91061a0
JPHardin <jacob.hardin@baxter-academy.org>
2016-11-21 11:08:22 -0500

    Roll-A-Ball

---
+++ Assets/FirstScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstScript : MonoBehaviour {
+	public Vector3 moveDirection;
+	public Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+		rigidBody.velocity = moveDirection;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += moveDirection;
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.CompareTag ("Paddle")) {
+			moveDirection = new Vector3 (-moveDirection.x, 0, moveDirection.z);
+		} else {
+			moveDirection = new Vector3 (moveDirection.x, 0, -moveDirection.z);
+		}
+
+	}
+}
